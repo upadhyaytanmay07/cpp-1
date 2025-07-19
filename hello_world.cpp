6e79376b522eeb3d3e58c0215e1dfd08b60e3b01
upadhyaytanmay07 <tanmayupadhyay1159@gmail.com>
2025-07-19 23:16:32 +0530

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,15 @@
+// entc-b2, PRN 24070123120, Tanmay Upadhyay
+
+// program to print hello world 
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World"<<endl;
+}
+
+/* output for this code
+
+Hello World
+
+*/
