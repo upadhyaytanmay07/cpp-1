5e0d8cc7b3d590f802a8cd1afab5a6ceec2b489c
upadhyaytanmay07 <tanmayupadhyay1159@gmail.com>
2025-07-19 23:49:25 +0530

    Create even_or_odd_num.cpp

---
+++ even_or_odd_num.cpp
@@ -0,0 +1,28 @@
+// entc b2, prn 24070123120
+
+// check for even or odd number
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a number: ";
+    cin>> num;
+    if(num%2==0)
+    {
+    cout<<"\nthe number is even ";
+    }
+    else 
+    {
+        cout<<"\nthe number is odd";
+    }
+}
+
+/* output for this code
+
+Enter a number: 56
+
+the number is even
+
+*/
